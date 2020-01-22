66bec3de11e8d5d1c667c8251bc2509abb36df66
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-22 03:00:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!"
+end
+
+def parrot(phrase)
+  phrase = "Squawk!"
+  puts parrot(phrase)
+end
+  
